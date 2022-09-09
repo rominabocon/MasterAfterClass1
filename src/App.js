627739309a6da8b3f36906562e8d42a6c4f3ca5a
Romina Bocon <rominabocon@hotmail.com>
2022-09-09 12:35:58 -0300

    adding route home

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import NavBar from './components/NavBar/NavBar.jsx';
 import ToDoContainer from './components/Todos/ToDoContainer.jsx';
 import ToDoDetailContainer from './components/ToDoDetail/ToDoDetailContainer.jsx';
 import ToDoForm from './components/ToDoForm/ToDoForm';
+import Home from './components/Home/Home';
 
 function App() {
   return (
   <>
     <NavBar/>
     <Routes>
+      <Route path='/' element={<Home/>}/>
       <Route path='/todos/' element={<ToDoContainer/>}/>
       <Route path='/todos/:status' element={<ToDoContainer/>}/>
       <Route path='/todo/:id' element={<ToDoDetailContainer/>}/>
